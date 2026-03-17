a054b1b0eb2e65a757e43382bfbb04e5fa132292
Jacson R. Sasse <jacson.sasse@hotmail.com>
2026-03-16 23:52:55 -0300

    feat: new method to test

--- src/helpers/register.ts
+++ src/helpers/register.ts
@@ -2,4 +2,5 @@ import Handlebars from "handlebars";
 
 export function registerHelpers(hbs: typeof Handlebars) {
   hbs.registerHelper("sayHello", (name: string) => `Hello ${name}`);
+  hbs.registerHelper("sayGoodbye", (name: string) => `Goodbye ${name}`);
 }
